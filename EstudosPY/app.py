f250f76a28bcd2b5289098e419e38be39aa2e4fa
Guilherme <fellipeguilherme37@gmail.com>
2023-11-25 04:11:18 -0300

    Enviando alguns estudos de Python

--- EstudosPY/app.py
+++ EstudosPY/app.py
@@ -1,5 +1,10 @@
 x = 5
 y = 25
 sum = (x + y)
-print("valor da soma é: ", sum)
+print("valor da soma e: ", sum)
 
+name = "Guilherme"
+me_age = 23
+print(f"Ola, sou {name} e tenho {me_age} anos")
+
+print("Olá, mundo!")

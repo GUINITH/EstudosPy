5e2daefa01582a445d49eb11123d9cc7c95eb919
Guilherme <fellipeguilherme37@gmail.com>
2023-12-03 12:22:23 -0300

    Adicionando nova pasta de Entrada, com objetivo de explicar a entrada de dados no Python e com alguns métodos e funções que podemos utilizar com essa linguagem.

---
+++ EstudosPY/Entradas/Entradas.py
@@ -0,0 +1,8 @@
+
+'''
+Exemplo utilizando o método str() que determina o tipo de texto, envelopado temos a função input() aonde o usuário vai digitar seu nome e ele será guardado variável nome_user. Por fim printamos para rodar o código e exibir o nome do usuário.
+'''
+
+nome_user = str(input("Digite seu nome: "))
+print("Ola", nome_user)
+

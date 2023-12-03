4de065ae1f19492a72d2bf289255c7caeac7843f
Guilherme <fellipeguilherme37@gmail.com>
2023-12-02 23:41:18 -0300

    Adicionando os tipos de variáveis permitidos pelo Python, que são: int,float,string e bolean

---
+++ EstudosPY/Variáveis/TypeVar.py
@@ -0,0 +1,16 @@
+#Variáveis podem ter diferentes tipos. Alguns tipos são considerados tipos primitivos, ou seja, eles são tipos de dados mais básicos que podem ser utilizados para compor outros tipos mais complexos. Em Python esses tipos levam os seguintes nomes:
+#int: números inteiros, ou seja, números sem parte decimal: 0, 5, -1, 1000
+peso_user = int(100)
+print(peso_user, "kg")
+#float: números reais, ou seja, números com parte decimal: 1.0, -2.7, 3.14
+user_altura = float(1.79)
+print("Minha altura e: ",user_altura)
+#str: cadeias de caracteres (strings), ou seja, dados textuais: 'Olá Mundo!', "eu tenho 18 anos"
+primeiro_codigo = "Ola, Mundo"
+print(primeiro_codigo)
+#bool: valores lógicos (booleanos), ou seja, apenas um entre dois valores possíveis: True ou false
+nota_student = 5
+print(5 == nota_student)
+#output: true
+print(6 == nota_student)
+# output:False

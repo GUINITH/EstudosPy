9c574ad9e90a75976f6535581d86c892e23a0d62
Guilherme <fellipeguilherme37@gmail.com>
2023-12-02 23:18:19 -0300

    Aplicando conceitos de variáveis no Python

---
+++ EstudosPY/Variáveis/var.py
@@ -0,0 +1,10 @@
+# Declaração de Uma váriavel
+# Variavel no Python não necessita de palavras chaves, apenas com identificador.
+# Sinal de igual(=), no Python significa receber um valor 
+# Para adicionar um valor, podemos adicinar numeros, strings, boleano, etc. 
+user_name = "Guilherme"
+user_age = 24
+print(user_name," tem", user_age,  "anos" )
+
+#Exe: Declare uma variável
+momy_name = "Katia"

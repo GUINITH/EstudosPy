3bfef33196da74b27045b047621dcff1cc3525ac
Guilherme <fellipeguilherme37@gmail.com>
2023-12-10 22:17:10 -0300

    Adição de um novo exercício em Python, utilizando condicionais if,elif(Senão-se)

--- EstudosPY/Condicionais/Condicionais.py
+++ EstudosPY/Condicionais/Condicionais.py
@@ -6,7 +6,7 @@ media_notas = (nota1 + nota2)/2
 if media_notas == 10.0 or media_notas >= 6.0:
     print("Aluno Aprovado")
 
-elif media_notas < 6.0: 
-    print("Aluno Reprovado")
+elif media_notas < 5.0: 
+    print("Aluno em recuperação")
 
 print("Sua média é: ", media_notas)
---
+++ EstudosPY/Condicionais/condiExe.py
@@ -0,0 +1,12 @@
+studen_name = 'Felipe'
+age_student = 14
+mensage_retorno = "";
+
+if age_student >= 18:
+    mensage_retorno = f"Sua idade e: {age_student} anos, vc e maior";
+    print(studen_name, mensage_retorno)
+
+elif age_student < 18:
+    mensage_retorno =f"Sua idade e: {age_student} anos vc e menor";
+    print(studen_name, mensage_retorno)
+

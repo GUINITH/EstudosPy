1ad264c1218ea59b2d45b5ed3ddce563874207f2
Guilherme <fellipeguilherme37@gmail.com>
2023-12-20 07:25:05 -0300

    Comentando minha primeira função

--- EstudosPY/Funções/Function.py
+++ EstudosPY/Funções/Function.py
@@ -1,6 +1,7 @@
 # criando a função
 def minha_primeira_funcao():
 	print('Olá Mundo')
-
+#### Primeira Função no Python
+	
 # o programa começa de verdade aqui:
 minha_primeira_funcao() # chamada para a função

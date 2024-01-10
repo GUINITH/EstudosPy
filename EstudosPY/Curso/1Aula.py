bd2b3c995dd31ab339dfed7562546439a23640b5
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-01-09 22:25:54 -0300

    Adicionando Exercícios do Curso!

---
+++ EstudosPY/Curso/1Aula.py
@@ -0,0 +1,7 @@
+
+# Print: Uma função para imprimir alguma coisa na tela
+print("Hello World")
+print("Seja Bem-Vindo ao Curso de Python do Coding Tank!")
+
+# Input: Uma função para receber dados digitados pelo úsuario.
+input("Digite sua idade?: ")
---
+++ EstudosPY/Curso/2Aula.py
@@ -0,0 +1,37 @@
+## Variáveis
+"""
+Variáveis são pequenos espaços de memória que separamos para armazenar um dado, que em seguida poderá ser acessado através de identificador único em nosso código.s
+
+
+# 1. Variáveis
+
+my_var = "Sheldom" # Criando uma Variável
+
+# 2. Exiba o Conteúdo da Variável
+print(my_var) # Saída: Sheldom, tipo: string
+
+idade = 24 #Saída: 24, tipo int
+print(idade)
+
+Peso = 135.5 # Saída: 135.5, tipo float
+print(Peso)
+
+Estudando = True
+Nao_estudando = False
+
+print(Estudando ) # Saída: True, tipo booleano
+print(Nao_estudando ) # Saída: False, tipo booleano
+
+#3 Exiba os tipos das variáveis
+print(type(my_var)) #String
+print(type(idade)) #Int
+print(type(Peso)) #Float
+print(type(Estudando)) #Bool
+print(type(Nao_estudando)) #Bool
+"""
+# Obtenha dados do úsuario, crie 3 variáveis, uma para guardar o nome, altura e peso do úsuario e exiba no terminal
+name_user = str(input("Qual seu nome?: "))
+user_age = int(input("Qual sua idade?: "))
+user_peso = float(input("Qual seu peso?:  "))
+
+print(f"Bem-vindo {name_user}, você tem {user_age} anos e pesa {user_peso} kg.")

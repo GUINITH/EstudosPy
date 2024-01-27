52c8508ce1ae299759e9646c8be24be3b017f5e4
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-01-27 00:06:33 -0300

    Adicionando conteúdo na pasta 5exerci.py

---
+++ EstudosPY/Exercícios/5execi.py
@@ -0,0 +1,12 @@
+"""contador_pipas = 0
+pipas = 50
+
+while contador_pipas < pipas:
+    print(contador_pipas)
+    contador_pipas+=1
+"""
+
+for num in range(10,110,10):
+    print(num)
+    # Output: Loop conta uma sequência númerica 10 até 100, ele conta de 10 em 10 ex"10,20,30,40,50..
+    

0ce7f9966bf3aaf19c7b001ff5123cb85bf5cded
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-01-29 21:14:09 -0300

    Novos exercicios adicionado na recém-criada pasta 6execi.py

---
+++ EstudosPY/Exercícios/6execi.py
@@ -0,0 +1,17 @@
+# Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+minha_mensagem = "Alou Mundo"
+print(minha_mensagem)
+
+# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+numero=int(input("digite um número: "))
+print(f"O número informado foi {numero}")
+
+# Faça um Programa que peça dois números e imprima a soma.
+
+numero1=int(input("digite um número: "))
+numero2=int(input("digite um número: "))
+soma= numero1 + numero2
+print(f"A soma dos números é: {soma}")
+

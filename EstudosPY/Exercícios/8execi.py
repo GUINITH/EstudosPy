100569f8c5fcfc8aeb0a52c3e199c91b8d78e304
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-02-03 22:21:49 -0300

    Adicionando uma nova pasta 8exercipy. Conteúdo: Listas em Python

---
+++ EstudosPY/Exercícios/8execi.py
@@ -0,0 +1,23 @@
+# Listas
+# Listas é tipo  de variáveis que podem receber diversos valores
+
+
+#Esse é o exemplo de lista declaração de listas:
+lista_exeplo1 = [1,2,3]
+#Se quiser utilizar o método list()
+lista_exeplo2=list(range(1,4,1))
+print(lista_exeplo1, lista_exeplo2)
+# lista vazia
+lista_vazia = []
+# Função list(), serve para converter/criar uma lista com essa função
+list()
+# Acessando inidices da minha lista
+#Crie uma lista e acesse o segundo indice da lista
+lista = list(range(2,11,2))
+# lista = [ 2,4,6,8,10]
+#Acessando o segundo índice de minha lista:
+print(lista[2]) # saída: 6
+#Pegando o último elemento da lista
+print(lista[-1]) # Resultado: 10
+#Pegando o penúltimo elemento da lista
+print(lista[-2])

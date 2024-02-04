0cb32411751eb3ec13ceecb7113f8a6bb6a5fe03
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-02-03 22:53:11 -0300

    Adicionando mais conteúdos de listas

--- EstudosPY/Exercícios/8execi.py
+++ EstudosPY/Exercícios/8execi.py
@@ -20,4 +20,26 @@ print(lista[2]) # saída: 6
 #Pegando o último elemento da lista
 print(lista[-1]) # Resultado: 10
 #Pegando o penúltimo elemento da lista
-print(lista[-2])
+print(lista[-2]) #Resultado: 8
+
+# Pegando ídices subjacentes com slice:
+lista_slice = [3,4,5,6,7,8,]
+# Pega os 3 primeiros índices da lista com slice:
+print(lista_slice[:3])
+# Pega os 3 últimos índices da lista com slice:
+print(lista_slice[3:])
+#Pegue os dois índices do meio da lista:
+print(lista_slice[2:4])
+# Além disso podemos adicionar passos no slice, para pegar uma sequência 2 em 2 por exemplo
+lista_mult2 = list(range(11))
+# pegue um múltiplo de 2 em uma lista até 10:
+# [valor inicial é o índice da sublista a ser gerado, segundo é o valor final, terceiro é o passo, ou seja de 2 em 2 números]
+print(lista_mult2[2:11:2]) # Resultado: [2,4,6,8,10]
+#pecorrendo lista
+for numero in lista_mult2:
+    print(numero)
+#Testando a existência de valores:
+#Declarando uma lista
+lista_nomes = [ "Guilherme", "Rafael","Pedro", "Lucas"]
+exist_nome = "Rafael" in lista_nomes
+print(exist_nome)

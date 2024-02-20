da8df27f0c9fb1713a612054330907a11752f11b
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-02-19 22:39:23 -0300

    Praticando leitura de arquivos

---
+++ EstudosPY/BootCIelo/3ARQUIVOS.py
@@ -0,0 +1,18 @@
+"""# Abrir um arquivo para leitura
+
+arquivo = open("./EstudosPY/BootCIelo/teste.txt", "r", encoding="utf-8")
+
+# ler o arquivo
+print(arquivo.read())
+
+#Fechando arquivo:
+arquivo.close()
+"""
+
+arquivo = open("teste2.txt", "w", encoding="utf-8")
+
+#Escrever no arquivo:
+arquivo.write("Guilherme Felipe")
+
+#Ler arquivo:
+print(arquivo.read())

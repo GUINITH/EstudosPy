535ea26852609decf7019f0adb6198b9ed87407f
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-03-04 22:43:59 -0300

    finalizando conteúdo de dicionários

--- EstudosPY/BootCIelo/4dict.py
+++ EstudosPY/BootCIelo/4dict.py
@@ -1,4 +1,4 @@
-jogador ={
+"""jogador ={
     "nome": "Zico",
     "idade": 76,
     "Nacionalidade": "Brasileiro",
@@ -61,5 +61,95 @@ print(dicio_clubs)
 
 #get
 print(f"Existe Corinthians: {dicio_clubs.get('Corinthians')}")
-print(dicio_clubs)
+print(dicio_clubs)"""
+
 #Setdefault
+dicio_alunos = {'nome1': "Pedro", "idade1": 23, 'nome2':"Miguel", "idade2": 43,'nome3':"Pablo", "idade3": 29 }
+novo_aluno = dicio_alunos.setdefault("nome4","Guilherme")
+novo_aluno = dicio_alunos.setdefault("idade4",24)
+print(dicio_alunos)
+
+#Copy
+
+"""Se você deseja obter todas as chaves de um dicionário, use o método keys():
+
+1
+2
+3
+computador = {'CPU': 'Intel', 'RAM': '8gb', 'SSD': '250bg'}
+
+print(computador.keys())
+A saída é um objeto do tipo dict_keys que equivale a uma lista com todas as chaves:
+
+dict_keys(['CPU', 'RAM', 'SSD'])
+Para obter apenas os valores das chaves em seu dicionário:
+
+1
+2
+3
+notas = {'Mat': 5, 'Por': 7, 'His': 8}
+
+print(notas.values())
+A saída será uma lista com os valores de cada chave:
+
+
+frutas = {'pera': 10, 'uva': 2, 'maça': 55}
+
+print(frutas.items())
+
+O método update() tanto pode criar, como atualizar dados:
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+dicio = {'nome': 'Erick'}
+
+# Atualiza o elemento de chave 'nome'
+dicio.update({'nome':'Matheus'})
+
+# Cria os elementos de chave 'idade' e 'tamanho'
+dicio.update({'idade':18})
+dicio.update(tamanho=1.60)
+
+print(dicio)
+
+Excluindo elementos no dicionário
+Você pode excluir um elemento do dicionário com a keyword del do Python, especificando sua chave:
+
+1
+2
+3
+4
+5
+pessoa = {'nome': 'Matheus', 'idade': 18, 'tamanho': 1.60}
+
+del pessoa['tamanho']
+
+print(pessoa)
+Resultando na exclusão do dado especificado:
+
+{'nome': 'Matheus', 'idade': 18}
+Excluindo e retornando o elemento excluído
+O método pop() remove o item cujo a chave foi especificada:
+
+1
+2
+3
+4
+5
+6
+sacola = {'maça': 2, 'ovos': 6, 'farinha': 2}
+
+ovos = sacola.pop('ovos')
+
+print(ovos)
+print(sacola)
+
+"""

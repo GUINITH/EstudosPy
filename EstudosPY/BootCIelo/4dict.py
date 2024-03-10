94c5d663989689b0a464f2915eaddc32fc39cb68
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-03-09 23:28:07 -0300

    fix: Comentando as linhas do código que estavão bugadas

--- EstudosPY/BootCIelo/4dict.py
+++ EstudosPY/BootCIelo/4dict.py
@@ -1,4 +1,5 @@
-"""jogador ={
+
+jogador ={
     "nome": "Zico",
     "idade": 76,
     "Nacionalidade": "Brasileiro",
@@ -41,7 +42,7 @@ print(dicio_pessoas)
 dicio_clubs = {"nome1": "Flamengo", 
                "nome2": "Vasco",
                "nome3": "Fluminense",
-               "nome4": "Botafogo",}
+               "nome4": "Botafogo"}
 
 # Atribuindo valores com as chaves novas e passando os valores
 dicio_clubs["nome5"] =  "Real Madrid"
@@ -71,28 +72,28 @@ print(dicio_alunos)
 
 #Copy
 
-Se você deseja obter todas as chaves de um dicionário, use o método keys():
+#Se você deseja obter todas as chaves de um dicionário, use o método keys():
 
 computador = {'CPU': 'Intel', 'RAM': '8gb', 'SSD': '250bg'}
 
 print(computador.keys())
-A saída é um objeto do tipo dict_keys que equivale a uma lista com todas as chaves:
+#A saída é um objeto do tipo dict_keys que equivale a uma lista com todas as chaves:
 
 dict_keys(['CPU', 'RAM', 'SSD'])
-Para obter apenas os valores das chaves em seu dicionário:
+#Para obter apenas os valores das chaves em seu dicionário:
 
 
 notas = {'Mat': 5, 'Por': 7, 'His': 8}
 
 print(notas.values())
-A saída será uma lista com os valores de cada chave:
+#A saída será uma lista com os valores de cada chave:
 
 
 frutas = {'pera': 10, 'uva': 2, 'maça': 55}
 
 print(frutas.items())
 
-O método update() tanto pode criar, como atualizar dados:
+#O método update() tanto pode criar, como atualizar dados:
 
 
 dicio = {'nome': 'Erick'}
@@ -106,19 +107,19 @@ dicio.update(tamanho=1.60)
 
 print(dicio)
 
-Excluindo elementos no dicionário
-Você pode excluir um elemento do dicionário com a keyword del do Python, especificando sua chave:
+#Excluindo elementos no dicionário
+#Você pode excluir um elemento do dicionário com a keyword del do Python, especificando sua chave:
 
 pessoa = {'nome': 'Matheus', 'idade': 18, 'tamanho': 1.60}
 
 del pessoa['tamanho']
 
 print(pessoa)
-Resultando na exclusão do dado especificado:
+#Resultando na exclusão do dado especificado:
 
 {'nome': 'Matheus', 'idade': 18}
-Excluindo e retornando o elemento excluído
-O método pop() remove o item cujo a chave foi especificada:
+#Excluindo e retornando o elemento excluído
+# método pop() remove o item cujo a chave foi especificada:
 
 sacola = {'maça': 2, 'ovos': 6, 'farinha': 2}
 
@@ -127,12 +128,12 @@ ovos = sacola.pop('ovos')
 print(ovos)
 print(sacola)
 
-Excluindo último item
-O método popitem() também remove o último elemento do dicionário.
+#Excluindo último item
+#O método popitem() também remove o último elemento do dicionário.
 
-Contudo, diferente do método pop() que apenas retorna o valor daquele elemento, popitem() retorna o elemento todo, contendo chave e valor.
+#Contudo, diferente do método pop() que apenas retorna o valor daquele elemento, popitem() retorna o elemento todo, contendo chave e valor.
 
-Veja usando o mesmo exemplo do método pop():
+#Veja usando o mesmo exemplo do método pop():
 
 sacola = {'maça': 2, 'ovos': 6, 'farinha': 2}
 
@@ -140,27 +141,27 @@ farinha = sacola.popitem()
 
 print(farinha)
 print(sacola)
-Veja que o retorno foi uma tupla e o último item foi removido:
+#Veja que o retorno foi uma tupla e o último item foi removido:
 
 
 ('farinha', 2)
 {'maça': 2, 'ovos': 6}
-Limpando dicionário
-Uma maneira de esvaziar um dicionário é utilizando o método clear(), dessa forma:
+#Limpando dicionário
+#Uma maneira de esvaziar um dicionário é utilizando o método clear(), dessa forma:
 
 
 dicio = {'nome': 'F9', 'motor': 'v8', 'ano': 2019}
 dicio.clear()
 
 print(dicio)
-O retorno é um dicionário vazio: {}.
+#O retorno é um dicionário vazio: {}.
 
-Copiando Dicionário
-Em Python, você não pode simplesmente digitar dict1 = dict2 para copiar um dicionário.
+#Copiando Dicionário
+#Em Python, você não pode simplesmente digitar dict1 = dict2 para copiar um dicionário.
 
-Dessa forma você esta apenas copiando a referência do dict1 ao dict2.
+#Dessa forma você esta apenas copiando a referência do dict1 ao dict2.
 
-A maneira correta de criar uma cópia do dicionário é utilizando o método copy(), assim:
+#A maneira correta de criar uma cópia do dicionário é utilizando o método copy(), assim:
 
 
 dicio = {"operacao": "web scraping", "dados": 250}
@@ -169,14 +170,16 @@ copia = dicio.copy()
 
 print(copia)
 print(dicio)
-Resultando na cópia exata do dicionário
+#Resultando na cópia exata do dicionário
 
 {'operacao': 'web scraping', 'dados': 250}
 {'operacao': 'web scraping', 'dados': 250}
-O método dict.setdefault()
-O método dict.setdefault() retorna o valor da chave especificada, e, caso a chave não exista, a cria com o valor especificado.
+#O método dict.setdefault()
+#O método dict.setdefault()  
 
-Vamos ao exemplo:
+#retorna o valor da chave especificada, e, caso a chave não exista, a cria com o valor especificado.
+
+#Vamos ao exemplo:
 
 dicio = {'coleta': 'scrapy', 'dados': 200}
 
@@ -184,11 +187,11 @@ set_dados = dicio.setdefault('dados')
 
 print(set_dados)
 print(dicio)
-Como a chave dados existe, ela é retornada:
+#Como a chave dados existe, ela é retornada:
 
 200
 {'coleta': 'scrapy', 'dados': 200}
-Agora vamos adicionar apenas a chave sem valor, e outro com chave e valor:
+#Agora vamos adicionar apenas a chave sem valor, e outro com chave e valor:
 
 
 dicio = {'coleta': 'scrapy', 'dados': 200}
@@ -199,16 +202,16 @@ set_teste = dicio.setdefault('teste', 1)
 print(set_data)
 print(set_teste)
 print(dicio)
-A saída resulta em:
+#A saída resulta em:
 
 
 {'coleta': 'scrapy', 'dados': 200, 'data': None, 'teste': 1}
-O set_data que teve apenas atribuída uma chave, teve seu valor criado como None, enquanto set_teste teve o valor adicionado.
+#O set_data que teve apenas atribuída uma chave, teve seu valor criado como None, enquanto set_teste teve o valor adicionado.
 
-Criando Dicionários a partir de lista de valores
-Com o método dict.fromkeys() é possível criar um dicionário a partir de uma lista de chaves e um valor, que será usado em todas as chaves.
+#Criando Dicionários a partir de lista de valores
+#Com o método dict.fromkeys() é possível criar um dicionário a partir de uma lista de chaves e um valor, que será usado em todas as chaves.
 
-Veja o exemplo:
+#Veja o exemplo:
 
 
 chaves = ['chave1', 'chave2', 'chave3']
@@ -216,4 +219,5 @@ valor = 0
 
 dicio = dict.fromkeys(chaves , valor)
 
-print(dicio)""""
+
+print(dicio)

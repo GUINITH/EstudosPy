406cd373b03199282926781733ec57be6f1d4c52
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-03-19 07:35:13 -0300

    Adicionando exercicio de compreesão de lista

--- EstudosPY/BootCIelo/6Lcompre.py
+++ EstudosPY/BootCIelo/6Lcompre.py
@@ -1,2 +1,7 @@
 cont_quadrados = [num**2 for num in range(1,11)]
 print(cont_quadrados)
+
+#Divisiveis por 2, no intervalo de entre 1 à 20:
+#Condicionais de Compreensão:
+divisiveis_por2 = [num for num in range(1,21) if num % 2 == 0]
+print(divisiveis_por2)

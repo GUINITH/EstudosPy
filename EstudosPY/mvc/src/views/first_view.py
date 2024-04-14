e49f6b9ed91b7feaddbea262fc6bf2c21317a462
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-04-14 14:23:57 -0300

    Subindo padrão de MVC

---
+++ EstudosPY/mvc/src/views/first_view.py
@@ -0,0 +1,13 @@
+def introduction_page():
+    mensagem = '''
+        Sistema Cadastral
+
+        * Cadastrar Pessoa -1
+        * Buscar Pessoa Por Nome * 2
+        * Sair 5
+    
+    '''
+    print(mensagem)
+    command = input('Comando: ')
+    return command 
+

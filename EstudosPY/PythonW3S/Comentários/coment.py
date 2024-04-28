75c127fa1b5764bf7beb77891d95b239e54142e0
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-04-27 23:01:07 -0300

    Subindo conteúdo de variáveis

---
+++ EstudosPY/PythonW3S/Comentários/coment.py
@@ -0,0 +1,10 @@
+# Introdução aos Comentários no Python
+
+# 1 - Para deixar um comentário no Python, utilizamos operador '#' para iniciar a linha de comentário e depois podemos deixar quailsquer comentários
+
+#2 O Python não tem comentários múltiplas-linhas assim como outras linguagens, mas você pode utilizar aspas triplas ''' ''', entre o espaço das mesma você pode deixar comentário de múltiplas-linhas:
+
+''' 
+Isso é um comentário multi-linha, basicamente é uma strig sem uma variável para armazená-la, desde que você use sem declarar uma variável para receber a mesma, o Python vai ignora-lá.
+
+''' 

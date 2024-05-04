aa0160bdd7a3b3004b35fbffc99feb2dc4600438
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-05-03 21:19:36 -0300

    Subindo exercicios de Python

---
+++ exec1.py
@@ -0,0 +1,10 @@
+
+# Calcule um aumento de salario de 15% 
+
+salario_user = int(input('Digite seu salario: '))
+aumento = salario_user * 0.15 
+total = aumento + salario_user 
+
+print(f'Seu salário é: {salario_user}')
+print(f'Aumento que você vai receber: {aumento}')
+print(f'O total é: {total}')

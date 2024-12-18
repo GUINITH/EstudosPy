55441de0c3eecbe3101e20967dc1fac6c6ea0996
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-12-17 22:06:30 -0300

    Estudos do curso Undemy

---
+++ Curso_undemy/1al.py
@@ -0,0 +1,12 @@
+# Comentários de uma linha com a cerquilha 
+
+"""
+Comentário de multiplas linhas utiliando Docstrings
+Comentário de multiplas linhas
+"""
+
+# Python ler de forma procedural, de cima para  baixo linha a linha da esquerda para a direita
+numeros = [1,2,3,4,5]
+for n in numeros:
+    print(n)
+
---
+++ Curso_undemy/2al.py
@@ -0,0 +1,10 @@
+# Print: 
+
+''' 
+Print é uma função que exibe algo na tela.
+Ele serve para exibir conteúdos de funções, variáveis, etc.
+Serve para exiber operações de argumetos não nomeados.
+Realiza operações para exibir resultados na tela.
+'''
+
+print("Hello World") 

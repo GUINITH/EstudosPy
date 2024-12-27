11789ab09d9aae383eabad25bb273e0c219d2bdc
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-12-27 20:14:00 -0300

    Estudos de operadores aritiméticos

---
+++ Curso_undemy/9al.py
@@ -0,0 +1,19 @@
+# Operadores Aritiméticos: adição, subitração, multiplicação, divisão e potência
+
+# Adição: +
+# Subitração: -
+# Multiplicação: *
+# Divisão_flutuante: /
+# Divisão_inteira: //
+# Resto da Divisão: %
+# Potência: **
+
+# Exemplos: 
+
+print(10 + 5) # 15
+print(10 - 5) # 5
+print(10 * 5) # 50
+print(10 / 5) # 2.0
+print(10 // 5) # 2
+print(10 % 5) # 0
+print(10 ** 2) # 100

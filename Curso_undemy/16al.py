75d6e0c05da99fc4f9af0d60bda27586d7f9e172
Guilherme Felipe <fellipeguilherme37@gmail.com>
2024-12-30 16:06:40 -0300

    Subindo exercicios

---
+++ Curso_undemy/16al.py
@@ -0,0 +1,20 @@
+"""
+Operadores de comparação: São operadores que comparam dois valores e o  sistema retorna um booleano (True ou False).
+
+Operadores de comparação são usados ​para comparar dois valores:
+
+- `==` : Igual a
+- `!=` : Diferente de
+- `>` : Maior que 
+- `<` : Menor que 
+- `=<` : Menor ou igual que
+- `>=` : Maior ou igual que
+
+"""
+
+print(23 == 23) # True
+print(23 != 23) # False
+print(23 > 23) # False
+print(23 < 23) # False
+print(23 >= 23) # True
+print(23 <= 23) # True 
---
+++ Curso_undemy/17al.py
@@ -0,0 +1,12 @@
+# Exerciicos de Operadors de comparação e condicionais
+
+
+primeiro_valor =  input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'Primeiro_valor={primeiro_valor} maior que segundo_valor={segundo_valor}')
+elif primeiro_valor < segundo_valor:
+    print(f'Primeiro_valor={primeiro_valor} menor que segundo_valor={segundo_valor}')
+else:
+    print(f'Primeiro_valor={primeiro_valor} igual ao segundo_valor={segundo_valor}')

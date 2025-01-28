454e2ca4def032309b825a549d57db4865eee92a
Guilherme Felipe <fellipeguilherme37@gmail.com>
2025-01-27 23:06:59 -0300

    Subindo exercicios de Python

---
+++ Curso_undemy/40al.py
@@ -0,0 +1,13 @@
+"""
+split e join com lista e str 
+join une uma string 
+split separa uma string e retorna uma lista
+strip retira os espacos em branco no inicio e no final
+
+"""
+
+frase = '  Olha que coisa mais linda tão cheia de graça   '
+frase2 = 'A coisa mais linda que ja vi passar'
+print(frase.join(frase2))
+print(frase.strip())
+print(frase.split())

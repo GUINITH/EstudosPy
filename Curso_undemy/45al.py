360d1d3c4400ad4cab9ee9692428cdf7d24118f4
Guilherme Felipe <fellipeguilherme37@gmail.com>
2025-02-05 20:53:35 -0300

    Definções de funções, argumentos

---
+++ Curso_undemy/45al.py
@@ -0,0 +1,11 @@
+# Funções
+
+def minha_funcao():
+    print("Hello World")
+
+print(minha_funcao())
+
+def execut(a, b,c): 
+    print(a + b + c)
+
+print(execut(1,2,3))
---
+++ Curso_undemy/47al.py
@@ -0,0 +1,17 @@
+# Argumentos Nomeados e Não nomeados 
+
+
+# Argumentos Nomeados 
+
+def soma(a =3, b=2):
+    return a + b
+
+print(soma())
+
+
+# Argumentos Não Nomeados
+
+def soma(a, b):
+    return a + b
+
+print(soma(3, 2))
